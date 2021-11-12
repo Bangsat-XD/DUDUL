9382f26f44613c2cd707732210ae9e05d81636a9
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-12 12:17:58 +0700

    Update SAYANG.py

--- SAYANG.py
+++ SAYANG.py
@@ -21,21 +21,21 @@ except ImportError:
 ### USERAGENT ###
 ua = ('Mozilla/5.0 (Linux; Android 5.0; Lenovo A1000 Build/S100; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/MESSENGER;FBAV/110.0.0.14.69;]')
 ### Logo ###
-logo = ("""            ───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
-            ───█▒▒░░░░░░░░░▒▒█───
-            ────█░░█░░░░░█░░█────
-            ─▄▄──█░░░▀█▀░░░█──▄▄─
-            █░░█─▀▄░░░░░░░▄▀─█░░█
-           █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-           █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
-           █░░║║║╠─║─║─║║║║║╠─░░█
-           █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
-           █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
-      ┏━┳━┓┏━━┓╋╋╋╋┏━━┓╋╋╋╋╋╋╋╋╋┏┓
-      ┃┃┃┃┣╋┓┏┻┳━┳┓┃┏┓┣━━┳━┓┏━┳┳┛┣━┓
-      ┃┃┃┃┃┃┃┃╋┃╋┃┗┫┣┫┃┃┃┃╋┗┫┃┃┃╋┃╋┗┓
-      ┗┻━┻╋┓┣┻━┻━┻━┻┛┗┻┻┻┻━━┻┻━┻━┻━━┛
-      ╋╋╋╋┗━┛     """)
+logo = ("""              ───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+              ───█▒▒░░░░░░░░░▒▒█───
+              ────█░░█░░░░░█░░█────
+              ─▄▄──█░░░▀█▀░░░█──▄▄─
+              █░░█─▀▄░░░░░░░▄▀─█░░█
+             █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+             █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+             █░░║║║╠─║─║─║║║║║╠─░░█
+             █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+             █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+        ┏━┳━┓┏━━┓╋╋╋╋┏━━┓╋╋╋╋╋╋╋╋╋┏┓
+        ┃┃┃┃┣╋┓┏┻┳━┳┓┃┏┓┣━━┳━┓┏━┳┳┛┣━┓
+        ┃┃┃┃┃┃┃┃╋┃╋┃┗┫┣┫┃┃┃┃╋┗┫┃┃┃╋┃╋┗┓
+        ┗┻━┻╋┓┣┻━┻━┻━┻┛┗┻┻┻┻━━┻┻━┻━┻━━┛
+        ╋╋╋╋┗━┛     """)
 mbasic_h={"Host":"mbasic.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
 free_h={"Host":"free.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
 mfb_h={'Host': 'm.facebook.com', 'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1', 'user-agent':ua, 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'accept-encoding': 'gzip, deflate', 'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'}
